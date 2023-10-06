f3360d9528a51baa322583289b4616249ed2d9ab
Anastasia <angu.work@mail.ru>
2023-10-06 22:56:34 +0300

    game brain even almost done

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -6,20 +6,26 @@ const greetUser = () => {
 };
 
 const isNumberEven = () => {
-  const randomNumber = Math.floor(Math.random() * 100);
-  console.log(`Question: ${randomNumber}`);
-  const answer = readlineSync.question('Your answer: ');
-  if (randomNumber % 2 === 0 && answer === 'yes') {
-    console.log('Correct!');
-  } else if (randomNumber % 2 === 0 && answer === 'no') {
-    console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}!`);
-  } else if (randomNumber % 2 !== 0 && answer === 'no') {
-    console.log('Correct!');
-  } else if (randomNumber % 2 !== 0 && answer === 'yes') {
-    console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
-  } else {
-    console.log(`Let's try again, ${name}!`);
+  for (let i = 0; i <= 2; i += 1) {
+    const randomNumber = Math.floor(Math.random() * 100);
+    console.log(`Question: ${randomNumber}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (randomNumber % 2 === 0 && answer === 'yes') {
+      console.log('Correct!');
+    } else if (randomNumber % 2 === 0 && answer === 'no') {
+      console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}!`);
+      return;
+    } else if (randomNumber % 2 !== 0 && answer === 'no') {
+      console.log('Correct!');
+    } else if (randomNumber % 2 !== 0 && answer === 'yes') {
+      console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
+      return;
+    } else {
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
   }
+  console.log(`Congratulations, ${name}!`);
 };
 
 export default isNumberEven;
