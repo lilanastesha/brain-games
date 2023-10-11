5040d52204a556529708962929e572e3c950a331
Anastasia <angu.work@mail.ru>
2023-10-11 14:56:14 +0300

    add game arithmetic progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,21 +1,5 @@
 import startOfGame from '../index.js';
 
-// const isProgression = () => {
-//   const startNum = Math.floor(Math.random() * 10) + 1;
-//   const step = Math.floor(Math.random() * 10) + 1;
-//   const arrayLength = Math.floor(Math.random() * 6) + 5;
-
-//   const array = [];
-//   for (let i = 0; i < arrayLength; i += 1) {
-//     array.push(startNum + (step * i));
-//   }
-
-//   const randomIndex = Math.floor(Math.random() * arrayLength);
-//   let missingNumber = array[randomIndex];
-//   missingNumber = 'х';
-//   return array;
-// };
-
 const roundOfGame = () => {
   const startNum = Math.floor(Math.random() * 10) + 1;
   const step = Math.floor(Math.random() * 10) + 1;
