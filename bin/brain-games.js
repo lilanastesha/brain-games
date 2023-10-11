b41b7a1b4b9f040e7785b8c14e4f7dffa08f5156
Anastasia <angu.work@mail.ru>
2023-10-11 20:36:29 +0300

    add brain-games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,13 +1,5 @@
 #!/usr/bin/env node
-// import isNumberEven from './brain-even.js';
-// import greetUser from '../src/cli.js';
-// import calc from './brain-calc.js';
 
-// console.log('Welcome to the brain games!');
+import greeting from '../src/cli.js';
 
-// greetUser();
-
-// console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-// isNumberEven();
-// calc();
+greeting();
